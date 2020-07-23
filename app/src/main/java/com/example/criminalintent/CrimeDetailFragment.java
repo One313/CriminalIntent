6bafc1f5a1c7bf487c948ea7b733143879b4f741
One313 <farahani.hr@gmail.com>
2020-07-23 17:55:35 +0430

    create and initialize Crime object for test in CrimeDetailFragment.java! create initViews() method!

--- app/src/main/java/com/example/criminalintent/CrimeDetailFragment.java
+++ app/src/main/java/com/example/criminalintent/CrimeDetailFragment.java
@@ -11,11 +11,15 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
+import com.example.criminalintent.model.Crime;
+
 public class CrimeDetailFragment extends Fragment {
 
     private EditText mEditTextCrimeTitle;
     private Button mButtonDate;
     private CheckBox mCheckBoxSolved;
+
+    private Crime mCrime;
 
 
     public CrimeDetailFragment() {
@@ -27,6 +31,7 @@ public class CrimeDetailFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        mCrime = new Crime();
     }
 
     @Override
@@ -35,7 +40,13 @@ public class CrimeDetailFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_crime_detail, container, false);
         findViews(view);
+        initViews();
         return view;
+    }
+
+    private void initViews() {
+        mButtonDate.setText(mCrime.getDate().toString());
+        mButtonDate.setEnabled(false);
     }
 
     private void findViews(View view) {
