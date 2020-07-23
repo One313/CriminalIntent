97bd18d5a41872c251425cc17c60c5bcce31c350
One313 <farahani.hr@gmail.com>
2020-07-23 23:28:27 +0430

    add setListener() method to onCreateView in CrimeDetailFragment.java include onCheckedChanged on mCheckBox and onTextChanged on mEditText!

--- app/src/main/java/com/example/criminalintent/CrimeDetailFragment.java
+++ app/src/main/java/com/example/criminalintent/CrimeDetailFragment.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 import com.example.criminalintent.model.Crime;
@@ -41,7 +44,34 @@ public class CrimeDetailFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_crime_detail, container, false);
         findViews(view);
         initViews();
+        setListeners();
         return view;
+    }
+
+    private void setListeners() {
+        mCheckBoxSolved.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
+            }
+        });
+
+        mEditTextCrimeTitle.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     private void initViews() {
